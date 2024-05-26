2b353f62368f04b38c4c74ffdda7a22fd17bf533
aleksanderesolo <163632389+aleksanderesolo@users.noreply.github.com>
2024-05-26 08:21:24 +0200

    Delete Beverage.java

--- Beverage.java
+++
@@ -1,9 +0,0 @@
-public abstract class Beverage {
-    String decription = "Unknown beverage";
-
-    public String getDescription() {
-        return decription;
-    }
-
-    public abstract double cost();
-}
